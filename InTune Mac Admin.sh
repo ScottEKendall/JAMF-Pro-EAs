4ab2ab578f62d66d9ff0bdb7c612d1d5704c6bf7
Scott Kendall <106453475+ScottEKendall@users.noreply.github.com>
2025-09-09 11:48:09 -0400

    EAs that are used in our environment

--- InTune Mac Admin.sh
+++ InTune Mac Admin.sh
@@ -1,11 +1,25 @@
 #!/bin/zsh
+run_for_each_user()
+{
+    SUPPORT_DIR="/Users/$1/Library/Application Support"
+    JSS_FILE="$SUPPORT_DIR/com.GiantEagleEntra.plist"
 
-LOGGED_IN_USER=$( scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ && ! /loginwindow/ { print $3 }' )
+    admin_rights=$(/usr/libexec/PlistBuddy -c "Print EntraAdminRights" "$JSS_FILE" 2>&1)
+    [[ "$admin_rights" == *"Does Not Exist"* ]] && admin_rights="Unknown"
+    if [[ "$userCount" -eq 1 ]]; then #only one user on the system
+        retval+=$admin_rights
+    else
+        retval+="$1 - $admin_rights\n"
+    fi
+}
 
-SUPPORT_DIR="/Users/$LOGGED_IN_USER/Library/Application Support"
-JSS_FILE="$SUPPORT_DIR/com.GiantEagleEntra.plist"
+# Main Script
 
-admin_rights=$(/usr/libexec/PlistBuddy -c "Print EntraAdminRights" "$JSS_FILE" 2>&1)
-[[ "$admin_rights" == *"Does Not Exist"* ]] && admin_rights="Unknown"
+declare retval=""
+declare userAccounts=($(dscl . list /Users | grep -v '^_' | grep -v 'daemon' | grep -v 'nobody' | grep -v 'root' | grep -v 'localmgr' ))
+declare userCount=${#userAccounts[@]}
 
-echo "<result>$admin_rights</result>"
+for user in $userAccounts; do
+  run_for_each_user $user
+done
+echo "<result>$retval</result>"
