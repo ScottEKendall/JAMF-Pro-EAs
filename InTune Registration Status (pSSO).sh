749342cdeda8aa94a7d45e339c297211746f02a6
Scott Kendall <106453475+ScottEKendall@users.noreply.github.com>
2026-02-05 12:24:30 -0500

    EAs that are used in our environment

--- InTune Registration Status (pSSO).sh
+++ InTune Registration Status (pSSO).sh
@@ -12,63 +12,82 @@
 #                   - added if block to Jamf result in order to preserve last server inventory data (@hkystar35)
 #                   - variable updates to keep consitent with function (@hkystar35)
 #                   - update function name to reflect function work; the function is not looping through each user (@hkystar35)
+# updated 02/04/2026 - Reworked pSSO status to show more concise information 
 
 additionalAccountsToExclude='|appleadmin|macadmin' #use ' quotes; empty or comment out if not used; leave leading | if used; only separate by | as this will be inserted into a regex
 
-get_device_compliance_registration_status() {
-  local loggedInUser="$1"
-  local userHome
-  local platformStatus
-  local plist
-  local jamfCA="/Library/Application Support/JAMF/Jamf.app/Contents/MacOS/Jamf Conditional Access.app/Contents/MacOS/JAMF Conditional Access"
+function runAsUser ()
+{
+   /bin/launchctl asuser $userUID sudo -iu $loggedInUser "$@"
+}
 
-  #get user home directory
-  local userHome=$(/usr/bin/dscl . read "/Users/$loggedInUser" NFSHomeDirectory | /usr/bin/awk -F ' ' '{print $2}')
+function get_device_compliance_registration_status() {
+    local loggedInUser="$1"
+    local userUID=$(/usr/bin/id -u $loggedInUser)
+    local userHome=$(/usr/bin/dscl . read "/Users/$loggedInUser" NFSHomeDirectory | /usr/bin/awk -F ' ' '{print $2}')
+    local jamfCA="/Library/Application Support/JAMF/Jamf.app/Contents/MacOS/Jamf Conditional Access.app/Contents/MacOS/JAMF Conditional Access"
 
-  #Check if registered via PSSO/SSOe first
-  local ssoStatus=$(/bin/launchctl asuser $( /usr/bin/id -u $loggedInUser ) /Library/Application\ Support/JAMF/Jamf.app/Contents/MacOS/Jamf\ Conditional\ Access.app/Contents/MacOS/Jamf\ Conditional\ Access getPSSOStatus | tr -d '()[]"' | sed -E 's/, /\n/g')
-  if [[ $ssoStatus == *"primary_registration_metadata_device_id"* ]]; then
-    #Check if jamfAAD registered too
-    AAD_ID=$(/usr/bin/defaults read  "$userHome/Library/Preferences/com.jamf.management.jamfAAD.plist" have_an_Azure_id)
-    if [[ $AAD_ID -eq "1" ]]; then
-      local ssoStatus=$(echo $ssoStatus  | sed -E 's/(extraDeviceInformation |AnyHashable|primary_registration_metadata_)//g')
-      #jamfAAD ID exists, and PSSO/SSOe registered. Return getPSSOStatus results
-      retval+="Registered - $userHome\nDetails:\n$ssoStatus"
-      return 0
+    # Check if registered via PSSO/SSOe first
+    local ssoStatus=$(runAsUser $jamfCA getPSSOStatus | tr -d '()[]"' | sed -E 's/, /\n/g')
+
+    if [[ $ssoStatus == *"primary_registration_metadata_device_id"* ]]; then
+        # Check if jamfAAD registered too
+        AAD_ID=$(/usr/bin/defaults read  "$userHome/Library/Preferences/com.jamf.management.jamfAAD.plist" have_an_Azure_id)
+        if [[ $AAD_ID -eq "1" ]]; then
+            AADstatus=$(echo $ssoStatus | /usr/bin/head -n1 | /usr/bin/tr -d '[:space:]')
+            ssoStatus=$(echo $ssoStatus  | sed -E 's/(extraDeviceInformation |AnyHashable|primary_registration_metadata_)//g')
+
+            # jamfAAD ID exists, and PSSO/SSOe registered. Return getPSSOStatus results and parse the info
+            field_upn=$(printf '%s\n' "$ssoStatus" | /usr/bin/awk -F "upn: " 'NF>1{print $2}')
+            upn=$(printf '%s\n' "$field_upn" | /usr/bin/grep -Eo '[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}'| /usr/bin/head -1)
+
+            # the UPN from the jamfAAD Command could be blank, so lets grab this from the Apple SSO command and construct it
+            if [[ -z $upn ]]; then
+                local raw_upn=$(runAsUser app-sso platform -s | grep '"upn"'| sed -E 's/.*"upn"[[:space:]]*:[[:space:]]*"([^"]*)".*/\1/')
+                raw_upn=${raw_upn//\\@/@}           # unescape \@
+                local email=${raw_upn/\\*}          # keep everything up to first '@'
+                local domain_part=${raw_upn#*@}     # everything after first '@'
+                upn="$email@${domain_part%@*}"      # append domain up to second '@'
+            fi
+
+            field_device=$(printf '%s\n' "$ssoStatus" | /usr/bin/awk -F "device_id: " 'NF>1{print $2; exit}')
+            device_id=$(printf '%s\n' "$field_device" | /usr/bin/grep -Eo '[0-9a-fA-F]{8}(-[0-9a-fA-F]{4}){3}-[0-9a-fA-F]{12}'| /usr/bin/head -1)
+
+            case "${AADstatus}" in
+                0 ) joinmethod="pSSO Not Enabled" ;;
+                1 ) joinmethod="pSSO Enabled not registered" ;;
+                2 ) joinmethod="pSSO Enabled and registered" ;;
+            esac
+            retval+="$joinmethod ($AADstatus) | ${upn:-NoUPNFound} | ${device_id:-NoDeviceIDFound} | $userHome"
+            return 0
+        fi
+        # SSOe/PSSO secure enclave registered but not jamfAAD registered
+        retval+="WPJ Key is in Secure Enclave, but AAD ID not acquired for user home: $userHome"
     fi
-    #SSOe/PSSO secure enclave registered but not jamfAAD registered
-    retval+="WPJ Key is in Secure Enclave, but AAD ID not acquired for user home: $userHome"
-  fi
 
-  #Fall back to legacy (Login Keychain) checks
-  #check if wpj private key is present
-  local WPJKey=$(/bin/launchctl asuser $( /usr/bin/id -u $loggedInUser ) "/usr/bin/security find-certificate -a -Z | /usr/bin/grep -B 9 "MS-ORGANIZATION-ACCESS" | /usr/bin/awk '/\"alis\"<blob>=\"/ {print $NF}' | /usr/bin/sed 's/\"alis\"<blob>=\"//;s/.$//'")
-  if [ ! -z "$WPJKey" ]
-  then
-    #WPJ key is present
-    #check if jamfAAD plist exists
-    local plist="$userHome/Library/Preferences/com.jamf.management.jamfAAD.plist"
-    if [ ! -f "$plist" ]; then
-      #plist doesn't exist
+    # Fall back to legacy (Login Keychain) checks  / check if WPJ private key is present
+    local WPJKey=$(runAsUser "/usr/bin/security find-certificate -a -Z | /usr/bin/grep -B 9 "MS-ORGANIZATION-ACCESS" | /usr/bin/awk '/\"alis\"<blob>=\"/ {print $NF}' | /usr/bin/sed 's/\"alis\"<blob>=\"//;s/.$//'")
+    # See if no key is present
+    [[ -z "$WPJKey" ]] && { etval+="Not Registered for user home $userHome"; return 0; }
+    # WPJ key is present, so do some more checking
+    # check if jamfAAD plist exists
+    if [ ! -f "$userHome/Library/Preferences/com.jamf.management.jamfAAD.plist" ]; then
+        # plist doesn't exist
         retval+="WPJ Key present, JamfAAD PLIST missing from user home: $userHome\nDevice ID: $WPJKey"
         return 0
     fi
 
-    #PLIST exists. Check if jamfAAD has acquired AAD ID
-    local AAD_ID=$(/usr/bin/defaults read  "$userHome/Library/Preferences/com.jamf.management.jamfAAD.plist" have_an_Azure_id)
+    # PLIST exists. Check if jamfAAD has acquired AAD ID
+    
     if [[ $AAD_ID -eq "1" ]]; then
-      #jamfAAD ID exists
-      retval+="Registered - $userHome\nDevice ID: $WPJKey"
-      return 0
+        # jamfAAD ID exists
+        retval+="Registered - $userHome\nDevice ID: $WPJKey"
+        return 0
     fi
 
     #WPJ is present but no AAD ID acquired:
     retval+="WPJ Key Present. AAD ID not acquired for user home: $userHome\nDevice ID: $WPJKey"
     return 0
-  fi
-
-  #no wpj key
-  echo "Not Registered for user home $userHome"
 }
 
 # Main Script
@@ -80,6 +99,4 @@ for user in $userAccounts; do
   retval+="\n"
 done
 
-if [[ -n $retval ]]; then
-  echo "<result>$retval</result>"
-fi
+[[ -n $retval ]] && echo "<result>$retval</result>"
